b767a834d699e75c0d39972ae5a5d60068e6ffe8
jusaza_oficial <almanzasasajuliandavid4@gmail.com>
2025-07-02 23:09:54 -0500

    Update Permisos_Usuarios_MYSQL_CodeGol.sql

--- BD/Permisos_Usuarios_MYSQL_CodeGol.sql
+++ BD/Permisos_Usuarios_MYSQL_CodeGol.sql
@@ -5,7 +5,8 @@
 CREATE USER 'jeffersoncas'@'localhost' IDENTIFIED BY 'jeffer123';
 CREATE USER 'miguelro'@'localhost' IDENTIFIED BY 'sofia123';
 CREATE USER 'sofialoza'@'localhost'IDENTIFIED BY 'miguel123';
-CREATE USER 'julianalam''@localhost'IDENTIFIED BY 'julian123'
+CREATE USER 'julianalam''@localhost'IDENTIFIED BY 'julian123';
+
 -- 2. Proporcionar el acceso requerido al usuario con la información que requiere.
 
 GRANT ALL PRIVILEGES ON * . * TO 'jeffersoncas'@'localhost'; 
@@ -18,10 +19,10 @@ FLUSH PRIVILEGES;
 
 -- 4. Como se vio en sesiones anteriores ingresamos al motor con el usuario nuevo por medio de la sentencia:
 
---mysql -h localhost -u jeffersoncas -p
+-- mysql -h localhost -u jeffersoncas -p
 
---mysql -h localhost -u miguelro -p
+-- mysql -h localhost -u miguelro -p
 
---mysql -h localhost -u sofialoza -p
+-- mysql -h localhost -u sofialoza -p
 
---mysql -h localhost -u julianalam -p
+-- mysql -h localhost -u julianalam -p
